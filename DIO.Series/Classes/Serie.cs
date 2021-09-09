d6627e0c9147289817062dc56e3ba5bb7d014ece
Amauri Cunha <proamauri@gmail.com>
2021-09-08 21:55:49 -0300

    init commit

---
+++ DIO.Series/Classes/Serie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series
+{
+    class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descricao: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano: " + this.Ano + Environment.NewLine;
+        }
+
+        public string RetornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public string RetornaId()
+        {
+            return this.Id;
+        }
+    }
+
+    
+}
