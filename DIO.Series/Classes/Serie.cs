ccea6158a536b23563fbf236ae49df1b50d292b1
Amauri Cunha <proamauri@gmail.com>
2021-09-08 23:21:44 -0300

    Evolution

--- DIO.Series/Classes/Serie.cs
+++ DIO.Series/Classes/Serie.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DIO.Series
 {
-    class Serie : EntidadeBase
+    public class Serie : EntidadeBase
     {
         private Genero Genero { get; set; }
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+        private bool Excluido { get; set; }
 
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
@@ -20,6 +18,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -29,6 +28,8 @@ namespace DIO.Series
             retorno += "Título: " + this.Titulo + Environment.NewLine;
             retorno += "Descricao: " + this.Descricao + Environment.NewLine;
             retorno += "Ano: " + this.Ano + Environment.NewLine;
+
+            return retorno;
         }
 
         public string RetornaTitulo()
@@ -36,9 +37,13 @@ namespace DIO.Series
             return this.Titulo;
         }
 
-        public string RetornaId()
+        public int RetornaId()
         {
             return this.Id;
+        }
+
+        public void Excluir() {
+            this.Excluido = true;
         }
     }
 
--- DIO.Series/Classes/SerieRepository.cs
+++ DIO.Series/Classes/SerieRepository.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DIO.Series.Interfaces;
 
 namespace DIO.Series
 {
-    public class SerieRepository : IRepository<T>
+    public class SerieRepository : IRepository<Serie>
     {
-       
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Excluir(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
